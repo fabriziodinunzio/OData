d572704e2978342663de7fe15a84a78c5a1f28c0
fabriziodinunzio <fabrizio.dinunzio1@hotmail.it>
2019-10-02 17:42:10 +0200

    Add files via upload

---
+++ MovieModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary
+{
+    //// Film
+    [DataContract(Name = "Movie", Namespace = "http://myprojects/oData")]
+    public class MovieModel
+    {
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; set; }
+        [DataMember]
+        [Required]
+        public virtual string Title { get; set; }
+
+        [DataMember]
+        public virtual DateTime ReleaseDate { get; set; }
+        [DataMember]
+        public virtual ICollection<ReviewModel> Reviews { get; set; }
+    }
+}
---
+++ ReviewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary
+{
+    [DataContract(Name = "Review", Namespace = "http://myprojects/oData")]
+    public class ReviewModel
+    {
+        [DataMember]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; set; }
+        [DataMember]
+        public virtual string Summary { get; set; }
+        [DataMember]
+        [Required]
+        public virtual string Reviewer { get; set; }
+        [DataMember]
+        public virtual string ReviewText { get; set; }
+        [DataMember]
+        public virtual MovieModel Movie { get; set; }
+    }
+}
