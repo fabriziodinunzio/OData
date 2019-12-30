35940fe2b7b260bc2498710cf25ff7eb3081591f
Fabrizio Di Nunzio <fabrizio.dinunzio1@hotmail.it>
2019-12-31 00:36:47 +0100

    MMMMMM

---
+++ ConsoleMovieServiceHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using MovieServiceLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMovieServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MovieService)))
+            {
+                Console.WriteLine(host.Description.Name);
+                foreach (var item in host.Description.Endpoints)
+                {
+                    Console.WriteLine(item.Address.Uri.AbsoluteUri);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
